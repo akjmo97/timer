3a6983c851c89e2dc8b2a6e85baf983e5f68abe3
akjmo97 <moro1905@hotmail.com>
2021-07-02 17:55:01 +0800

    implement timer

--- timer/timer.py
+++ timer/timer.py
@@ -1,6 +1,19 @@
+import time
+
 def add(x, y):
     return x + y
 
 
 def mul(a, b):
     return a * b
+
+
+class Timer:
+    def __init__(self):
+        self.time = 0
+
+    def start(self):
+        self.time = time.time()
+
+    def stop(self):
+        self.time = time.time() - self.time
